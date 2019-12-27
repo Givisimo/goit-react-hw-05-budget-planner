a01c3dc0d512c779115ddfd3341d8f975df61f72
Denys Plietnikov <givisimo@gmail.com>
2019-12-27 15:19:10 +0200

    Updates

---
+++ precache-manifest.2e83bb72ff2e98285e9358e4b257a0f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc5691430943cc09d4cc500f42b4261",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "10ad00b7d9de723f6bec",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.0f54525f.chunk.css"
+  },
+  {
+    "revision": "856b09c38c44672f818f",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.d48e5cf6.chunk.js"
+  },
+  {
+    "revision": "10ad00b7d9de723f6bec",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.60fd7095.chunk.js"
+  },
+  {
+    "revision": "fa343405502b5dfb228a",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.5f43b742.js"
+  }
+]);
